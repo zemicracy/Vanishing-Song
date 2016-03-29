54d80eb90f1659be1fc793bbcd1f544a3e9304ac
shana <shana@GM2140136.da.iwasaki.ac.jp>
2016-03-30 00:04:09 +0900

    no message

--- VanishingSong/VanishingSong/GearFrame.h
+++ VanishingSong/VanishingSong/GearFrame.h
@@ -11,17 +11,19 @@ public:
 
 
 private:
-	std::shared_ptr<Gear> m_pWaist;
-	std::shared_ptr<Gear> m_pRightShoulder;
-	std::shared_ptr<Gear> m_pLeftShoulder;
-	std::shared_ptr<Gear> m_pRightUpperArm;
-	std::shared_ptr<Gear> m_pLeftUpperArm;
-	std::shared_ptr<Gear> m_pRightLowerArm;
-	std::shared_ptr<Gear> m_pLeftLowerArm;
-	std::shared_ptr<Gear> m_pRightUpperLeg;
-	std::shared_ptr<Gear> m_pLeftUpperLeg;
-	std::shared_ptr<Gear> m_pRightLowerLeg;
-	std::shared_ptr<Gear> m_pLeftLowerLeg;
-	
+	struct{
+		std::shared_ptr<Gear> m_pWaist;
+		std::shared_ptr<Gear> m_pRightShoulder;
+		std::shared_ptr<Gear> m_pLeftShoulder;
+		std::shared_ptr<Gear> m_pRightUpperArm;
+		std::shared_ptr<Gear> m_pLeftUpperArm;
+		std::shared_ptr<Gear> m_pRightLowerArm;
+		std::shared_ptr<Gear> m_pLeftLowerArm;
+		std::shared_ptr<Gear> m_pRightUpperLeg;
+		std::shared_ptr<Gear> m_pLeftUpperLeg;
+		std::shared_ptr<Gear> m_pRightLowerLeg;
+		std::shared_ptr<Gear> m_pLeftLowerLeg;
+	};
+
 };
 
