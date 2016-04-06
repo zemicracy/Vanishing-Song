7c7e00b1b36fa9ea1c0d477dd8308dc3cf603c38
ydakoukn <ydakoukun0105@gmail.com>
2016-04-06 10:35:59 +0900

    橋本、金子の箇所を統合済み

--- VanishingSong/VanishingSong/SceneSurvival.h
+++ VanishingSong/VanishingSong/SceneSurvival.h
@@ -3,6 +3,9 @@
 #include "ShaderBase.h"
 #include "EnemyGround.h"
 #include "Player.h"
+#include "FieldArea.h"
+#include "OrderList.h"
+#include "ActionBoard.h"
 class SceneSurvival :
 	public aetherClass::GameScene
 {
@@ -33,5 +36,8 @@ private:
 	std::shared_ptr<EnemyGround> m_penemyGround;
 	std::shared_ptr<aetherClass::ShaderBase> m_pixelShader;
 	std::unique_ptr<Player> m_pPlayer;
+	std::unique_ptr<FieldArea> m_pFieldArea;
+	std::unique_ptr<OrderList> m_pOrderList;
+	std::unique_ptr<ActionBoard> m_pActionBoard;
 };
 
