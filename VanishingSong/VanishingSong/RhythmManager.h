dfd2d0fb6845e6969a10797ab9e77250954c2de7
kaneko <iwgm14cyancle@gmail.com>
2016-05-13 11:38:21 +0900

    Rhythmまねーじゃ作った

---
+++ VanishingSong/VanishingSong/RhythmManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include<GameSound.h>
+#include<memory>
+class RhythmManager
+{
+public:
+	RhythmManager();
+	~RhythmManager();
+
+	//AccesserMethod
+	bool mIsQuarterBeat();	//‚S•ª
+	bool mIsEighterBeat();	//‚W•ª
+	bool mIsWholeBeat();	//‘S‰¹•„
+
+	float mGetPlayTime();
+	
+	
+	void mInitializeRhythm(std::shared_ptr<aetherClass::GameSound>,int bpm);
+	void mAcquire();
+private:
+	std::shared_ptr<aetherClass::GameSound>m_sound;
+	float m_playTime;
+	
+	int m_bpm;
+	int m_prevQuarterBeat;
+	int m_prevEighterBeat;
+	int m_prevWholeBeat;
+
+	bool m_IsQuarterBeat;
+	bool m_IsEighterBeat;
+	bool m_IsWholeBeat;
+};
+
