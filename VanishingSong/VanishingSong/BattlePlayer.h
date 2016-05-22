aa94afbaa3a49daffeef9e71093a7b0205a1ff50
ydakoukn <ydakoukun0105@gmail.com>
2016-05-22 23:48:32 +0900

    戦闘のプレイヤークラスをちょっと作った

--- VanishingSong/VanishingSong/BattlePlayer.h
+++ VanishingSong/VanishingSong/BattlePlayer.h
@@ -1,16 +1,25 @@
 #ifndef _BATTLEPLAYER_H
 #define _BATTLEPLAYER_H
 #include "Const.h"
+#include "CharaEntity.h"
+#include "GearFrame.h"
+#include <ShaderBase.h>
 class BattlePlayer
 {
 public:
 	BattlePlayer();
 	~BattlePlayer();
 
-	void Initialize(eMusical);
+	void mInitialize(eMusical,std::shared_ptr<GearFrame> gearframe);
 	eMusical mGetType();
+	void mRender(aetherClass::ShaderBase*);
+	void mUpdate(const float);
+	void mSetPosition(aetherClass::Vector3);
 private:
 	eMusical m_type;
+	CharaEntity m_charaEntity;
+	std::shared_ptr<GearFrame> m_playerGear;
+	aetherClass::Transform m_transform;
 };
 
 #endif
