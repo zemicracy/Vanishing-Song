821f5f5953852fc5d8c637271a026546d3d72dc3
kaneko <iwgm14cyancle@gmail.com>
2016-06-19 22:37:26 +0900

    フォルダ分け

--- VanishingSong/VanishingSong/ResourceManager.cpp
+++ VanishingSong/VanishingSong/ResourceManager.cpp
@@ -78,11 +78,11 @@ bool ResourceManager::InitializeBGM(){
 	アクションコマンドに対応する音の初期化
 */
 bool ResourceManager::InitializeActionSound(){
-	RegisterActionSound(eMusical::eBlue, "Sound/blue.wav");
-	RegisterActionSound(eMusical::eGreen, "Sound/green.wav");
-	RegisterActionSound(eMusical::eRed, "Sound/red.wav");
-	RegisterActionSound(eMusical::eYellow, "Sound/yellow.wav");
-	RegisterActionSound(eMusical::eMiss, "Sound/miss.wav");
+	RegisterActionSound(eMusical::eBlue, "Sound\\Command\\blue.wav");
+	RegisterActionSound(eMusical::eGreen, "Sound\\Command\\green.wav");
+	RegisterActionSound(eMusical::eRed, "Sound\\Command\\red.wav");
+	RegisterActionSound(eMusical::eYellow, "Sound\\Command\\yellow.wav");
+	RegisterActionSound(eMusical::eMiss, "Sound\\Command\\miss.wav");
 
 	return true;
 }
