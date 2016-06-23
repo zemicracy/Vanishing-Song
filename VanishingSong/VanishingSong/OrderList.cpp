6bf3568cb471580e340952de6d8d3fe8954cd7af
kaneko <iwgm14cyancle@gmail.com>
2016-06-23 14:02:03 +0900

    エフェクト4

--- VanishingSong/VanishingSong/OrderList.cpp
+++ VanishingSong/VanishingSong/OrderList.cpp
@@ -229,9 +229,9 @@ void OrderList::mInitialize(GameManager::eGameMode mode,GameManager::eBattleStat
 
 	m_pEffect->mLoadEffect(9, "Break", "Effect\\BreakCommand\\");
 	m_pEffect->mLoadEffect(6, "In", "Effect\\InCommand\\");
-	m_pEffect->mLoadEffect(7, "Miss", "Effect\\Miss\\");
-	m_pEffect->mLoadEffect(7, "Good", "Effect\\Good\\");
-	m_pEffect->mLoadEffect(7, "Great", "Effect\\Great\\");
+	m_pEffect->mLoadEffect(17, "Miss", "Effect\\Miss\\");
+	m_pEffect->mLoadEffect(17, "Good", "Effect\\Good\\");
+	m_pEffect->mLoadEffect(17, "Great", "Effect\\Great\\");
 
 
 	m_effectTrans = Transform(Vector3(), Vector3(), Vector3( 16*10, 9*10,0));
