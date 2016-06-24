812b049df0ac553a2a0f8262148645d8d4f45574
ydakoukn <ydakoukun0105@gmail.com>
2016-06-25 03:08:06 +0900

    ロード画面の作りこみ ラインを出した

--- VanishingSong/VanishingSong/Load.cpp
+++ VanishingSong/VanishingSong/Load.cpp
@@ -7,24 +7,27 @@
 #include <WorldReader.h>
 #include <GameController.h>
 using namespace aetherClass;
+
 namespace{
 	const float kChangeBarTime = 0.3f;
+	const float kWaveMaxValue = 0.45f;
 }
+
 Load::Load()
 {
 	m_changeBarTime = NULL;
 	m_barCount = NULL;
 }
 
-
+//
 Load::~Load()
 {
 	m_changeBarTime = NULL;
 	m_barCount = NULL;
 }
 
+//
 void Load::Initialize(){
-	bool result;
 	m_pLoadBar = std::make_unique<Rectangle2D>();
 	m_pLoadBar->Initialize();
 
@@ -36,6 +39,7 @@ void Load::Initialize(){
 		}
 	}
 
+	//
 	m_model = std::make_shared<FbxModel>();
 	m_model->LoadFBX("Model\\Load\\model.fbx", eAxisSystem::eAxisOpenGL);
 	m_model->SetTextureDirectoryName("Model\\Load\\tex");
@@ -45,16 +49,72 @@ void Load::Initialize(){
 	m_model->property._transform._translation._y = -40;
 	m_model->property._transform._translation._x = 40;
 	m_view.property._translation._z = -100;
+
+	// メイン
+	m_waveTexture.Load("Texture\\Load\\wave.png");
+	m_pWave = std::make_shared<Rectangle2D>();
+	m_pWave->Initialize();
+	m_pWave->property._transform._scale = Vector3(kWindowWidth, kWindowHeight, 0);
+	m_pWave->SetTexture(&m_waveTexture);
+
+	// マスク
+	m_waveTextureMask.Load("Texture\\Load\\wave_mask.png");
+	m_pWaveMask = std::make_shared<Rectangle2D>();
+	m_pWaveMask->Initialize();
+	m_pWaveMask->property._transform._scale = Vector3(kWindowWidth, kWindowHeight, 0);
+	m_pWaveMask->SetTexture(&m_waveTextureMask);
+
+	// 
+	ShaderDesc desc;
+	desc._pixel._srcFile = L"Shader/HalfFiller.hlsl";
+	desc._vertex._srcFile = L"Shader/VertexShaderBase.hlsl";
+	desc._pixel._entryName = "ps_main";
+	desc._vertex._entryName = "vs_main";
+
+	m_pWaveShader = std::make_shared<HalfFillShader>();
+	m_pWaveShader->Initialize(desc, eVertex | ePixel);
+
+	//
+	m_direcType._direction = Vector2(1, 0);
+	m_direcType._interpolation = 1;
+	m_pWaveShader->_property = m_direcType.mCastFillType();
+	m_originInterpolation = NULL;
+	m_maskInterpolation = NULL;
+	m_changeDirection = false;
+
+	return;
 }
 
-
+//
 void Load::Finalize(){
+	
+	//
 	if (m_pLoadBar){
 		m_pLoadBar->Finalize();
 		m_pLoadBar.reset();
 		m_pLoadBar = nullptr;
 	}
 
+	//
+	if (m_pWave){
+		m_pWave->Finalize();
+		m_pWave.reset();
+		m_pWave = nullptr;
+	}
+
+	//
+	if (m_pWaveMask){
+		m_pWaveMask->Finalize();
+		m_pWaveMask.reset();
+		m_pWaveMask = nullptr;
+	}
+
+	//
+	if (m_pWaveShader){
+		m_pWaveShader->Finalize();
+		m_pWaveShader.reset();
+		m_pWaveShader = nullptr;
+	}
 }
 
 /*
@@ -67,9 +127,12 @@ void Load::Run(){
 	m_model->KeyframeAnimationRender(shader["texture"].get());
 
 	mChangeBar(m_changeBarTime);
+	
 	DirectXEntity entity;
 	entity.GetDirect3DManager()->Change2DMode();
-	m_pLoadBar->Render(shader["texture"].get());
+
+	mUIRender(shader["texture"].get(), m_pWaveShader);
+	
 	entity.GetDirect3DManager()->Change3DMode();
 	
 	m_changeBarTime += GameClock::GetDeltaTime();
@@ -79,6 +142,7 @@ void Load::Run(){
 	
 }
 
+//
 bool Load::WaitRun(){
 	Run();
 	DirectXEntity entity;
@@ -88,10 +152,12 @@ bool Load::WaitRun(){
 		return kWaitting;
 	}
 	entity.GetDirect3DManager()->Change3DMode();
+	mResetProperty();
 	return kWaitEnd;
 
 }
 
+//
 void Load::mChangeBar(float& time){
 	if (time > kChangeBarTime){
 		m_barCount += 1;
@@ -108,4 +174,47 @@ void Load::mChangeBar(float& time){
 void Load::mResetProperty(){
 	m_changeBarTime = NULL;
 	m_barCount = NULL;
+	m_originInterpolation = NULL;
+	m_maskInterpolation = NULL;
+	m_changeDirection = false; 
+	return;
+}
+
+//
+void Load::mUIRender(ShaderBase* texture, std::shared_ptr<HalfFillShader>& shader){
+	m_pLoadBar->Render(texture);
+	if (m_changeDirection){
+		m_originInterpolation += GameClock::GetDeltaTime();
+		m_maskInterpolation = kWaveMaxValue;
+	}
+	else{
+		m_maskInterpolation += GameClock::GetDeltaTime();
+		m_originInterpolation = kWaveMaxValue;
+	}
+	
+
+	// 状態によって描画順を変える
+	if (m_changeDirection){
+		m_pWaveShader->_property._interpolation = m_maskInterpolation;
+		m_pWaveMask->Render(shader.get());
+
+		m_pWaveShader->_property._interpolation = m_originInterpolation;
+		m_pWave->Render(shader.get());
+	}
+	else{
+
+		m_pWaveShader->_property._interpolation = m_originInterpolation;
+		m_pWave->Render(shader.get());
+
+		m_pWaveShader->_property._interpolation = m_maskInterpolation;
+		m_pWaveMask->Render(shader.get());
+	}
+	
+
+	if (m_maskInterpolation > kWaveMaxValue || m_originInterpolation > kWaveMaxValue){
+		m_originInterpolation = NULL;
+		m_maskInterpolation = NULL;
+		m_changeDirection = !m_changeDirection;
+	}
+	return;
 }
--- VanishingSong/VanishingSong/Load.h
+++ VanishingSong/VanishingSong/Load.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <SpriteBase.h>
 #include <FbxModel.h>
+#include "HalfFillShader.h"
 class Load :
 	public aetherClass::LoadScreen
 {
@@ -19,12 +20,25 @@ public:
 private:
 	void mChangeBar(float& time);
 	void mResetProperty();
+	void mUIRender(aetherClass::ShaderBase*,std::shared_ptr<HalfFillShader>&);
 private:
 	std::unique_ptr<aetherClass::SpriteBase> m_pLoadBar;
+	std::shared_ptr<aetherClass::SpriteBase> m_pWave;
+	std::shared_ptr<aetherClass::SpriteBase> m_pWaveMask;
+	std::shared_ptr<HalfFillShader> m_pWaveShader;
+	aetherClass::Texture m_waveTexture;
+	aetherClass::Texture m_waveTextureMask;
+
 	std::shared_ptr<aetherClass::FbxModel> m_model;
+
 	float m_changeBarTime;
 	int m_barCount;
 	aetherClass::ViewCamera m_view;
+
+	HalfFillShader::DirectionGaugeType m_direcType;
+	float m_originInterpolation;
+	float m_maskInterpolation;
+	bool m_changeDirection;
 };
 
 #endif
