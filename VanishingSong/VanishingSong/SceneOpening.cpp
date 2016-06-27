386496efbd6f7629a76bf47c4589beaea0113c1e
yoshizumi <iwgm14kyoshizumi@gmail.com>
2016-06-27 16:58:46 +0900

    オープニング修正 BGMついてる

--- VanishingSong/VanishingSong/SceneOpening.cpp
+++ VanishingSong/VanishingSong/SceneOpening.cpp
@@ -34,19 +34,21 @@ bool SceneOpening::Initialize(){
 	m_pSpriteBase->property._transform._scale._y = 720;
 	m_pSpriteBase->property._color = Color(0, 0, 0, 1);
 
-	//時間カウント用変数
+	//BGM
+	m_openingSound = std::make_shared<GameSound>();
+	m_openingSound->Load("Sound\\Opening\\Opening.wav");
+	m_openingSound->SetValume(-3000);
+
+	//テクスチャ切り替え用に使っている変数の初期化
 	m_clockCount = 0;
-
-	//配列番号の切り換え用変数
 	m_array = 0;
-
-	//テクスチャ表示繰り返し用変数
+	m_openingflg = 0;
 	m_imageCount = 0;
 
 	//テクスチャを読み込み
 	for (int i = 0; i<17; i++){
 		m_pTexture[i] = std::make_unique<Texture>();
-		m_pTexture[i]->Load("Texture/Opening" + std::to_string(i + 1) + ".png");
+		m_pTexture[i]->Load("Texture/Opening/Opening" + std::to_string(i + 1) + ".png");
 	}
 
 	//1枚目はすぐ表示
@@ -65,33 +67,87 @@ bool SceneOpening::Updater(){
 	//m_clockCountにデルタタイムを足していく
 	m_clockCount = m_clockCount + GameClock::GetDeltaTime();
 
-	//1、2繰り返し表示
-	if (m_imageCount < 2){
-		if (m_clockCount > 1){
+	m_openingSound->PlayToOneTime();
+
+	//1秒ごとに1、2繰り返し表示
+	if (m_imageCount < 3){
+		if (m_clockCount > 0.7){
 			m_clockCount = 0;
 			if (m_array < 2){
 				m_pSpriteBase->SetTexture(m_pTexture[m_array].get());
 				m_array++;
 			}
 		}
-		//m_arrayが2以上なら
 		if (m_array>1){
 			m_array = 0;
 			m_imageCount++;
 		}
 	}
-	if (m_imageCount == 2){
+	//1、2繰り返し表示を止めるように入れてる処理です
+	if (m_imageCount == 3){
 		m_array = 2;
 	}
+	
+	//2秒ごとに3～9表示
+	if (m_openingflg == 0 && m_imageCount > 2){
+		if (m_clockCount > 2){
+			m_clockCount = 0;
+			if (m_array < 9){
+				m_pSpriteBase->SetTexture(m_pTexture[m_array].get());
+				m_array++;
+				m_imageCount++;
+			}
+		}
+	}
 
-	//3～17表示
-	if (m_imageCount > 1){
+	if (m_imageCount == 10){
+		m_openingflg = 1;
+	}
+
+	//10を1秒表示
+	if (m_openingflg == 1 && m_imageCount > 9){
+		if (m_clockCount > 1){
+			m_clockCount = 0;
+			if (m_array < 10){
+				m_pSpriteBase->SetTexture(m_pTexture[m_array].get());
+				m_array++;
+				m_imageCount++;
+			}
+		}
+	}
+	
+	//11、12繰り返し表示するほうに処理がいくように入れてます
+	if (m_imageCount == 11){
+		m_openingflg = 2;
+	}
+
+	//11、12繰り返し表示
+	if (m_openingflg == 2 && m_imageCount > 10){
+		if (m_clockCount > 0.5){
+			m_clockCount = 0;
+			if (m_array < 12){
+				m_pSpriteBase->SetTexture(m_pTexture[m_array].get());
+				m_array++;
+			}
+		}
+		if (m_array>11){
+			m_array = 10;
+			m_imageCount++;
+		}
+	}
+
+	//11、12繰り返し表示する処理を止めるために入れてます
+	if (m_imageCount == 12){
+		m_openingflg = 3;
+	}
+
+	//13～17表示
+	if (m_openingflg == 3){
 		if (m_clockCount > 2){
 			m_clockCount = 0;
 			if (m_array < 17){
 				m_pSpriteBase->SetTexture(m_pTexture[m_array].get());
 				m_array++;
-				m_imageCount++;
 			}
 		}
 	}
--- VanishingSong/VanishingSong/SceneOpening.h
+++ VanishingSong/VanishingSong/SceneOpening.h
@@ -8,6 +8,7 @@
 #include<Texture.h>
 #include<memory>
 #include<GameClock.h>
+#include"GameSound.h"
 
 class SceneOpening :
 	public aetherClass::GameScene
@@ -16,12 +17,24 @@ private:
 	std::unique_ptr<aetherClass::ShaderBase> m_pShaderBase;
 	std::unique_ptr<aetherClass::SpriteBase>m_pSpriteBase;
 
+	//BGM用
+	std::shared_ptr<aetherClass::GameSound> m_openingSound;
+
 	//テクスチャ用の配列
 	std::unique_ptr<aetherClass::Texture>m_pTexture[17];
 
+	//時間カウント用変数
 	double m_clockCount;
+
+	//配列番号の切り換え用変数
 	int m_array;
+
+	//テクスチャ表示繰り返し用変数
 	int m_imageCount;
+
+	//テクスチャ繰り返し表示と続けて表示するパターン、切り替え用の変数
+	int m_openingflg;
+
 public:
 	SceneOpening();
 	~SceneOpening();
